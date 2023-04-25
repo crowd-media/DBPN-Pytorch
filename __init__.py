df67f56588284f8fb0fa955f0c5c72e94dbf63e6
a96lex <a96lex@gmail.com>
2023-04-25 13:16:57 +0000

    change: print -> logging

--- __init__.py
+++ __init__.py
@@ -2,8 +2,9 @@
 
 import os
 import sys
+import logging
 
-print("run init in dbpn")
+logging.info("[dbpn] run init in dbpn")
 
 ## add current dir to sys path so that root dbpn py files can be imported from outside 
 current_dir = os.path.dirname( __file__ )
